37388a409993e6d6bd69436ce09420584d4e7b2f
kimlai <kimlai@lrqdo.fr>
2018-07-30 18:56:43 +0200

    Basic tutorial in place.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as R from 'ramda';
+import classNames from 'classnames';
+
+const major = ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'];
+const penta = ['Bb', 'Db', 'Eb', 'F', 'Ab'];
+const blues = ['Bb', 'Db', 'Eb', 'E', 'F', 'Ab'];
+const harmonic = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+const functions = ['1', null, '2', '3m', '3M', '4', null, '5', null, '6', '7m', '7M', '1'];
+
+const noteFunction = (i) => {
+    return R.nth(i, functions);
+};
+
+const Scale = ({ scale, highlights = [] }) => {
+    const [before, after] = R.splitWhen(R.equals(R.head(scale)), harmonic);
+    const notes = R.concat(R.concat(after, before), [R.head(scale)]);
+
+    return (
+        <div className='scale'>
+            {notes.map((note, i) => (
+                <div key={i}>
+                    <div className='noteFunction'>{noteFunction(i)}</div>
+                    <div
+                        className={classNames(
+                            'note',
+                            { inScale: R.contains(note, scale),
+                              highlighted: R.contains(note, highlights),
+                            }
+                        )}
+                    >
+                        {note}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>I got rhythm</h1>
+                <h2>Gamme majeure</h2>
+                <p>
+                    La première partie du thème (le A) utilise uniquement les notes de la
+                    gamme majeure de Si bémol (noté Bb en anglais).
+                </p>
+                <p>
+                    Ci-dessous on voit les notes qui composent la gamme, ainsi que la "fonction"
+                    de chacune des notes dans la gamme (1 c'est la fondamentale, 2 la seconde, 3
+                    la tierce etc.). Le "m/M" indique si c'est majeur ou mineur, on voit que seules
+                    la tierce ou la 7ème peuvent être majeure ou mineure, les autres sont "justes".
+                </p>
+                <Scale scale={major} />
+                <audio src='/Bb-major-scale.m4a' controls />
+                <h2>Le thème</h2>
+                <p>
+                    Là j'ai simplement surligné les notes utilisées sur le thème.
+                </p>
+                <Scale scale={major} highlights={['Bb', 'C', 'F', 'G', 'D']} />
+                <h2>Gamme pentatonique mineure</h2>
+                <p>
+                    Pour certaines des variations, Sara Vaughan utilise la gamme pentatonique
+                    mineure.
+                </p>
+                <p>
+                    Les deux différences avec la gamme majeure sont
+                </p>
+                <ul>
+                    <li>Pas de seconde ni de sixième (penta = 5 notes)</li>
+                    <li>La tierce et la septième sont mineures</li>
+                </ul>
+                <Scale scale={penta} />
+                <audio src='/Bb-penta-scale.m4a' controls />
+                <h2>Gamme blues</h2>
+                <p>
+                    En réalité Sara Vaughan utilise la gamme "blues" et pas la pentatonique mineure.
+                </p>
+                <p>
+                    C'est simplement la gamme pentatonique mineure avec en plus ce qu'on appelle la
+                    "note blues", ici c'est un Mi.
+                </p>
+                <Scale scale={blues} highlights={['E']} />
+                <audio src='/Bb-blues-scale.m4a' controls />
+                <h2>Exercices</h2>
+                <p>
+                    Un bon exercice pour s'assurer qu'on a bien les gammes en tête, c'est de les transposer.
+                    Partir de n'importe quelle note (par exemple Fa) et réciter la gamme de Fa majeur.
+                </p>
+                <p>
+                    Ça sert à s'assurer qu'on a bien assimilé les intervalles de la gamme, et pas juste
+                    appris la gamme de Si bémol par coeur.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
